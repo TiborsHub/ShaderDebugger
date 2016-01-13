b0e3c8960a076b9994d9a19c1db78bf41b0f2f2d
Tibor den Ouden <tibordenouden@gmail.com>
2016-01-13 14:59:28 +0100

    ShaderStructureState added

---
+++ LibShaderDebugger/Interface/ShaderStructureState.h
@@ -0,0 +1,38 @@
+/*******************************************************************************\
+*                                                                               *
+*           Class   : ShaderStructureState                                      *
+*           Purpose : Contains all locations of nodes where the execution path  *
+*                     can branch and the state of the branch variable           *
+*           File    : ShaderStructureState.h                                    *
+*           Author  : Tibor den Ouden                                           *
+*                                                                               *
+\*******************************************************************************/
+
+
+#pragma once
+
+
+#ifndef __SHADER_STRUCTURE_STATE_H__
+#define __SHADER_STRUCTURE_STATE_H__
+
+
+// Library headers
+#include "ASTLocation.h"
+
+
+// Standard headers
+#include <vector>
+
+
+class ShaderStructureState
+{
+    tASTLocation                mMain;
+    std::vector<tASTLocation>   mSelections;
+    std::vector<tASTLocation>   mBranches;
+    std::vector<tASTLocation>   mLoops;
+    std::vector<tASTLocation>   mFunctionDefinitions;
+    std::vector<tASTLocation>   mFunctionCalls;
+};
+
+
+#endif // __SHADER_STRUCTURE_STATE_H__
