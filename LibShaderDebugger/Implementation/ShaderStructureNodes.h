6a6e9e211f69a8974874e8b52b3ab735f61665b1
Tibor den Ouden <tibordenouden@gmail.com>
2016-01-13 13:09:21 +0100

    ShaderStructureNodes added

---
+++ LibShaderDebugger/Implementation/ShaderStructureNodes.h
@@ -0,0 +1,35 @@
+/*******************************************************************************\
+*                                                                               *
+*           Class   : ShaderStructureNodes                                      *
+*           Purpose : Information about all nodes at which the shader excution  *
+*                     path can deviate from sequencial execution                *
+*           File    : ShaderStructureNodes.h                                    *
+*           Author  : Tibor den Ouden                                           *
+*                                                                               *
+\*******************************************************************************/
+
+
+#pragma once
+
+
+#ifndef __SHADER_STRUCTURE_NODES_H__
+#define __SHADER_STRUCTURE_NODES_H__
+
+
+// Library headers
+#include "ASTNodeLocation.h"
+
+
+class ShaderStructureNodes
+{
+public:
+    tASTNodeLocation              mMain;
+    std::vector<tASTNodeLocation> mSelections;
+    std::vector<tASTNodeLocation> mBranches;
+    std::vector<tASTNodeLocation> mLoops;
+    std::vector<tASTNodeLocation> mFunctionDefinitions;
+    std::vector<tASTNodeLocation> mFunctionCalls;
+};
+
+
+#endif // __SHADER_STRUCTURE_NODES_H__
